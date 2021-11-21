db1d21b3f546c06f2a947a32d2550bdd530a47f6
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2021-11-21 12:10:43 -0500

    Task Service experimenting with Junit Tests

---
+++ SNHU Task/src/test/Task.java
@@ -0,0 +1,44 @@
+package test;
+
+public class Task {
+	// declarations task ID will be final 
+	private final String taskId; 
+	private String name;
+	private String description;
+	
+	public Task (String taskId, String name, String description) {
+
+		if (taskId == null || taskId.length() > 10) {
+			throw new IllegalArgumentException("Invalid ID");
+		}
+
+		if (name == null || name.length() > 20) {
+			throw new IllegalArgumentException("Invalid name");
+		}
+
+		if (description == null || description.length() > 50) {
+			throw new IllegalArgumentException("Invalid description");
+		}
+			
+		this.taskId = taskId;
+		this.name = name;
+		this.description = description;
+	}
+	// Getter Methods
+	public String getTaskId () {
+		return taskId;
+	}
+	public String getName () {
+		return name;
+	}
+	public String getDescription () {
+		return description;
+	}
+	// Setter Methods
+	public void setName(String newName) {
+		name = newName;
+	}
+	public void setDescription(String newDescription) {
+		description = newDescription;
+	}
+}
---
+++ SNHU Task/src/test/TaskTest.java
@@ -0,0 +1,59 @@
+package test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TaskTest {
+
+	@Test
+	void testTaskClass() {
+		Task task = new Task("56783", "Name", "Dull task Decription");
+		assertTrue(task.getTaskId().equals("56783"));
+		assertTrue(task.getName().equals("Name"));
+		assertTrue(task.getDescription().equals("Dull task Decription"));
+	}
+		
+	@Test
+	void testTaskClassTaskIdTooLong() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Task("567836789123", "Name", "Dull task Decription");
+		});
+	}
+	
+	@Test
+	void testTaskClassTaskIdNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Task(null, "Name", "Dull task Decription");
+		});
+	}
+
+	@Test
+	void testTaskClassNameTooLong() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Task("56783", "This name is too long", "Dull task Decription");
+		});
+	}
+	
+	@Test
+	void testTaskClassNameNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Task("56783", null, "Dull task Decription");
+		});
+	}
+	
+
+	@Test
+	void testTaskClassDescriptionTooLong() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Task("56783", "Name", "This description is too long and should throw an exception");
+		});
+	}
+	
+	@Test
+	void testTaskClassDescriptionNull() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new Task("56783", "FirstName", null);
+		});
+	}
+}
